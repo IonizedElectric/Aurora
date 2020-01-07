9b1ca86c36c1b7dbeb03e33dbc9666eff9415cd9
Impossible-Inc <Impossible-Inc@users.noreply.github.com>
2020-01-07 20:11:31 +0000

    [BUG] Added GraphJs and installed some functions for saving (not implemented)

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-
+import { Chart } from 'chart.js';
 import { HomePage } from './home.page';
-
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+@Component({
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
+})
 @NgModule({
   imports: [
     CommonModule,
@@ -20,4 +25,41 @@ import { HomePage } from './home.page';
   ],
   declarations: [HomePage]
 })
+export class HomePage implements OnInit {
+  @ViewChild("lineCanvas") lineCanvas: ElementRef;
+  private lineChart: Chart;
+  constructor() {}
+  ngOnInit() {
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: "line",
+      data: {
+        labels: ["blnk", "blnk", "blnk", "blnk", "blnk", "Yesterday", "Today"],
+        datasets: [
+          {
+            //label: "My First dataset",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+            spanGaps: false
+          }
+        ]
+      }
+    });
+  }
+}
 export class HomePageModule {}
