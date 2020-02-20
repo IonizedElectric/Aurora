360a2da678a79471b67419aba7bc9d0b05d279a2
IonizedElectric <IonizedElectric@users.github.com>
2020-02-20 20:22:24 +0000

    [BUG][BUILD_FAIL][CHARLIE-GRADE PROBLEM]I really have no clue anymore. Someone help me.

--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { CalendarEvent, CalendarView } from 'angular-calendar';
+
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.page.html',
   styleUrls: ['./history.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HistoryPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  }
+    }
+    view: CalendarView = CalendarView.Month;
+
+    viewDate: Date = new Date();
+
+    events: CalendarEvent[] = [];
 
 }
